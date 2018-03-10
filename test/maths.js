cfed25867eb421a8b75fa404811bfa8025ab21bb
Stephanos Theodotou <stephanos.theodotou@gmail.com>
2018-03-10 18:37:37 +0000

    re-install packages and add package.json

--- test/maths.js
+++ test/maths.js
@@ -7,6 +7,9 @@ chai.should();
 function addition(num1, num2){
     return num1 + num2;
 }
+function division(num1, num2){
+    return num1 / num2;
+}
 
 describe("mathematical formulas", function(){
 
@@ -15,6 +18,9 @@ describe("mathematical formulas", function(){
   describe("addition", function(){
     it("should equal 10 when passing 6", function(){
       addition(num, 6).should.equal(10);
+    })
+    it("should not equal 14 when passing 9", function(){
+      addition(num, 9).should.not.equal(9);
     })
   })
 })
